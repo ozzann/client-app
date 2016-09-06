44c3ebe013fb0b7455da4b71fc0437acd0640e82
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-09-06 14:43:24 +0100

    wrap program in an infinite loop

--- src/MyClient.java
+++ src/MyClient.java
@@ -8,44 +8,42 @@ import java.net.Socket;
 public class MyClient {
 
     public static void main(String[] args){
-        System.out.print("Please print server's name:");
-        String input = System.console().readLine();
-        String serverName;
-        if( input != null && !input.isEmpty()){
-            serverName = input;
-        }
-        else{
-            System.out.println("Localhost is server's name by default");
-            serverName = "localhost"; // default server's name
-        }
+        while(true) {
+            System.out.print("Please print server's name:");
+            String input = System.console().readLine();
+            String serverName;
+            if (input != null && !input.isEmpty()) {
+                serverName = input;
+            } else {
+                System.out.println("Localhost is server's name by default");
+                serverName = "localhost"; // default server's name
+            }
 
-        System.out.print("Please print server's port:");
-        int port = 9090;  // default
-        try {
-            port = Integer.parseInt(System.console().readLine());
-        }
-        catch(NumberFormatException e){
-            System.out.println("Port should be a number");
-            System.out.println("Port is assgined to 9090 by default");
-        }
+            System.out.print("Please print server's port:");
+            int port = 9090;  // default
+            try {
+                port = Integer.parseInt(System.console().readLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Port should be a number");
+                System.out.println("Port is assgined to 9090 by default");
+            }
 
-        try
-        {
-            System.out.println("Connecting to " + serverName +
-                    " on port " + port);
-            Socket client = new Socket(serverName, port);
-            System.out.println("Just connected to "
-                    + client.getRemoteSocketAddress());
+            try {
+                System.out.println("Connecting to " + serverName +
+                        " on port " + port);
+                Socket client = new Socket(serverName, port);
+                System.out.println("Just connected to "
+                        + client.getRemoteSocketAddress());
 
-            DataInputStream in =
-                    new DataInputStream(client.getInputStream());
-            System.out.println("Server says " + in.readUTF());
-            client.close();
-        }catch(IOException e) {
-            e.printStackTrace();
+                DataInputStream in =
+                        new DataInputStream(client.getInputStream());
+                System.out.println("Server says " + in.readUTF());
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-
-        System.console().readLine();
 
     }
 }
